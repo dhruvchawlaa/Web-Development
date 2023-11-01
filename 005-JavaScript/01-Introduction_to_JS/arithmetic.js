6dfd8f1e86373f167cd3e13e45090d46b4dbd91c
dhruvchawlaa <dhruvv.chawlaa@gmail.com>
2023-11-01 11:20:15 -0400

    Added arithmetic.js

---
+++ 005-JavaScript/01-Introduction_to_JS/arithmetic.js
@@ -0,0 +1,18 @@
+console.log(9 + 10); // 19
+console.log(10 - 3); // 7
+console.log(6 * 9); // 54
+console.log(8 / 3); // 2.666...
+console.log(12 % 5); // 2
+
+// Check whether a number is divisible by 3
+var num = 9;
+if (num % 3 == 0) {
+  console.log(num + " is divisible by 3.");
+} else {
+  console.log(num + " is not divisible by 3.");
+}
+
+// Increment and Decrement
+var y = 7;
+y--; // y = y - 1 OR y -= 1
+console.log(y);
