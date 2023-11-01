f6c2bdfcf9ccb6dca8b02127545dd052a56be5a7
dhruvchawlaa <dhruvv.chawlaa@gmail.com>
2023-11-01 11:48:28 -0400

    Added functions.js

---
+++ functions.js
@@ -0,0 +1,16 @@
+// Creating a function
+function yoda() {
+  console.log("You must unlearn what you have learned.");
+}
+
+// Calling the function
+yoda(); // You must unlearn...
+
+//----------------------------------------------------------------
+
+// Creating a function with parameters
+function vader(name) {
+  console.log("I find your lack of faith disturbing, " + name + ".");
+}
+
+vader("Dhruv"); //...of faith disturbing, Dhruv.
