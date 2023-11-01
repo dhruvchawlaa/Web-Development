ccdb427c5899c8a694596654fc89a1105a161ad9
dhruvchawlaa <dhruvv.chawlaa@gmail.com>
2023-11-01 11:29:33 -0400

    Added variables.js

---
+++ 005-JavaScript/01-Introduction_to_JS/variables.js
@@ -0,0 +1,14 @@
+var myName = "Dhruv";
+console.log(myName); // Dhruv
+myName = "Dhruv Chawla";
+console.log(myName);
+console.log("My name is " + myName + ".");
+
+// Swapping two numbers
+var a = "3";
+var b = "8";
+var temp = a;
+a = b;
+b = temp;
+console.log("a is " + a);
+console.log("b is " + b);
