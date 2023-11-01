e445031d4f55451761140d0b8feb7583f8b25d10
dhruvchawlaa <dhruvv.chawlaa@gmail.com>
2023-11-01 11:40:19 -0400

    Added strings.js

---
+++ 005-JavaScript/01-Introduction_to_JS/strings.js
@@ -0,0 +1,15 @@
+// Concatenation
+var greeting = "Hello" + " there!";
+console.log(greeting); // Hello there!
+
+// Length
+console.log(greeting.length); // 12
+
+// Slicing and Extracting Parts of a String
+var tweet = "Fear is the path to the dark side.";
+var slice = tweet.slice(2, 6);
+console.log(slice); // ar is
+
+// Changing Casing
+console.log(tweet.toUpperCase()); // FEAR IS THE...
+console.log(tweet.toLowerCase()); // fear is the...
