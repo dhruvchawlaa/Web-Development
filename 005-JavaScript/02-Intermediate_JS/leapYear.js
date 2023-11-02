1f781ee2b7a69e901287f75d0ee6010f53029bbd
dhruvchawlaa <dhruvv.chawlaa@gmail.com>
2023-11-02 10:40:55 -0400

    Added leapYear.js

---
+++ 005-JavaScript/02-Intermediate_JS/leapYear.js
@@ -0,0 +1,36 @@
+// Method 1
+function isLeap(year) {
+  /**************Don't change the code above****************/
+
+  //Write your code here.
+  if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+    console.log("Leap year.");
+  } 
+  else if (year % 4 === 0 && year % 100 !== 0) {
+    console.log("Leap year.");
+  } 
+  else if (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) {
+    console.log("Not leap year.");
+  } 
+  else {
+    console.log("Not leap year.");
+  }
+
+  /**************Don't change the code below****************/
+}
+
+// Method 2
+function isLeap(year) {
+  /**************Don't change the code above****************/
+
+  //Write your code here.
+  if ((year % 4 === 0 && year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)
+  ) {
+    console.log("Leap year.");
+  } 
+  else {
+    console.log("Not leap year.");
+  }
+
+  /**************Don't change the code below****************/
+}
