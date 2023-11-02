a1fadb3a1431c773b7d5f11db23176084e6dc2e7
dhruvchawlaa <dhruvv.chawlaa@gmail.com>
2023-11-02 11:57:49 -0400

    Added whosBuyingLunch.js

---
+++ 005-JavaScript/02-Intermediate_JS/whosBuyingLunch.js
@@ -0,0 +1,10 @@
+function whosPaying(names) {
+  /******Don't change the code above*******/
+
+  //Write your code here.
+  var index = Math.floor(Math.random() * names.length);
+  var value = names[index] + " is going to buy lunch today!";
+  return value;
+
+  /******Don't change the code below*******/
+}
