77c307e390904bb6ecf18535859392f7ced15f35
dhruvchawlaa <dhruvv.chawlaa@gmail.com>
2023-11-08 10:59:07 -0500

    Manipulating styles with jQuery

--- 005-JavaScript/05-jQuery/index.js
+++ 005-JavaScript/05-jQuery/index.js
@@ -1,2 +1,21 @@
+// Selecting all the buttons
+$("button");
 
+// Changing heading color with jQuery
 $("h1").css("color", "red");
+
+// Getting the value of a style
+console.log($("h1").css("color"));
+console.log($("h1").css("font-size"));
+
+// Adding a class to the selected element
+$("h1").addClass("big-title");
+
+// Removing  a class
+$("h1").removeClass("big-title");
+
+// Adding multiple classes
+$("h1").addClass("big-title margin-50");
+
+// Checking whether the element has a class
+$("h1").hasClass("margin-50");
